e9f9c0520449652beb8b9114b507a08a6aab07fa
Richard Si <63936253+ichard26@users.noreply.github.com>
2021-08-13 22:24:01 -0400

    Hopefully this is a good reproducer

---
+++ non-pep621/bugfree.py
@@ -0,0 +1,3 @@
+"""The dependencies are installed even if flit is doing an editable install :D"""
+
+__version__ = "21.8a1"
---
+++ pep621/buggy.py
@@ -0,0 +1,3 @@
+"""The dependencies are NOT installed if flit is doing an editable install D:"""
+
+__version__ = "21.8a1"
